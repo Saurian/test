3578489b2b61088fd6c8144bfeb981cd24469dfa
Pavel <pavel.paulik@seznam.cz>
2015-02-07 15:49:42 +0100

    comin2

---
+++ test/Pokus2.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * This file is part of the smazat2
+ *
+ * Copyright (c) 2015
+ *
+ * @file Pokus2.php
+ * @author  Pavel Paulík <pavel.paulik1@gmail.com>
+ */
+
+class Pokus2 {
+
+
+
+
+//    naše soubory
+
+
+
+
+
+} 
