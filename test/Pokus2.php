bed4f85dc82ae078e93770f3225ec0c17bcad4dc
Pavel <pavel.paulik@seznam.cz>
2015-02-07 16:13:28 +0100

    Pokus 2

---
+++ test/Pokus2.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * This file is part of the smazat2
+ *
+ * Copyright (c) 2015
+ *
+ * @file Pokus2.php
+ * @author  Pavel Paulík <pavel.paulik1@gmail.com>
+ */
+
+class Pokus2 {
+
+
+
+
+//    naše soubory
+
+
+    private $test;
+
+    function __construct()
+    {
+        // TODO: Implement __construct() method.
+        $this->test = 0;
+    }
+
+
+} 
